758804bb0318e32bfc05113bdc3b2dd824240a22
UnrulyMob <72175425+JPM709@users.noreply.github.com>
2021-06-26 15:24:34 -0230

    deployment test

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,34 +1,34 @@
-import React, {useState, useEffect} from "react";
-import {Button, Card, Form} from "react-bootstrap";
+import React, {useEffect, useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {Button, Card, Form} from "react-bootstrap";
 
-const CENTER_SPACING_IMPERIAL = 16;
-const CENTER_SPACING_METRIC = 406.4;
-const STUD_OFFSET_IMPERIAL = 3 / 4;
-const STUD_OFFSET_METRIC = 19;
+const CENTER_SPACING_IMPERIAL = 16,
+  CENTER_SPACING_METRIC = 406.4,
+  STUD_OFFSET_IMPERIAL = 0.75,
+  STUD_OFFSET_METRIC = 19,
+  makeAList = (wallLength, isImperialUnit) => {
+    const BASE_STUD = 0,
+      newArray = [BASE_STUD],
+      onCenterSpacing = isImperialUnit ? CENTER_SPACING_IMPERIAL : CENTER_SPACING_METRIC,
+      studOffset = isImperialUnit ? STUD_OFFSET_IMPERIAL : STUD_OFFSET_METRIC;
+    for (
+      let studCount = 1;
+      studCount < Math.ceil(wallLength / onCenterSpacing);
+      studCount++
+    ) {
+      newArray.push(studCount * onCenterSpacing - studOffset);
+    }
+    newArray.push(wallLength - 2 * studOffset);
+    if (isImperialUnit) {
+      return newArray;
+    }
+    return newArray.map((roundedItem) => Math.round(roundedItem));
+  };
 
-const makeAList = (wallLength, isImperialUnit) => {
-  const studOffset = isImperialUnit 
-    ? STUD_OFFSET_IMPERIAL 
-    : STUD_OFFSET_METRIC;
-  const onCenterSpacing = isImperialUnit
-    ? CENTER_SPACING_IMPERIAL
-    : CENTER_SPACING_METRIC;
-
-  let newArray = [0];
-  for (let i = 1; i < Math.ceil(wallLength / onCenterSpacing); i++) {
-    newArray.push(i * onCenterSpacing - studOffset);
-  }
-  newArray.push(wallLength - 2 * studOffset);
-  if (isImperialUnit) {
-    return newArray;
-  } else {return newArray.map((x) => Math.round(x));}
-};
-
-const Calculator = () => {
-  const [listOfMeasurements, setListOfMeasurements] = useState([]);
-  const [isImperialUnit, setImperialUnit] = useState(true);
-  const [wallLength, setWallLength] = useState(0);
+function Calculator() {
+  const [listOfMeasurements, setListOfMeasurements] = useState([]),
+    [isImperialUnit, setImperialUnit] = useState(true),
+    [wallLength, setWallLength] = useState(0);
 
   useEffect(() => {
     setListOfMeasurements(makeAList(wallLength, isImperialUnit));
@@ -38,47 +38,55 @@ const Calculator = () => {
     setImperialUnit((prevUnit) => !prevUnit);
   }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setListOfMeasurements(makeAList(wallLength, isImperialUnit));
-  };
-
   const handleInputChange = (event) => {
     setWallLength(event.target.value);
-  };
+    },
+    handleSubmit = (event) => {
+    event.preventDefault();
+    setListOfMeasurements(makeAList(wallLength, isImperialUnit));
+    }
+    
 
   return (
     <>
       <Card>
         <Card.Title>Wall Stud Calculator</Card.Title>
+
         <Form onSubmit={handleSubmit}>
           <Form.Label>Wall length</Form.Label>
+
           <Form.Control
-            type="number"
             min="0"
             name="wallX"
-            value={wallLength}
             onChange={handleInputChange}
             required
+            type="number"
+            value={wallLength}
           />
-          <Button variant="primary" type="submit">
+
+          <Button type="submit" variant="primary">
             Layout wall
           </Button>
+
           <Card.Text>
             Now measuring in {isImperialUnit ? "Inches" : "Milimetres"}.
           </Card.Text>
-          <Button variant="warning" onClick={toggleUnits}>
+
+          <Button onClick={toggleUnits} variant="warning">
             Swap Between Imperial and Metric
           </Button>
         </Form>
       </Card>
+
       <Card>
         <h2>Directions:</h2>
+
         <p>
           You need {listOfMeasurements.length} studs. Don&apos;t forget, you will need 3
           more boards for your top and bottom plates for every
           {isImperialUnit ? " 96 inches" : " 2438 milimetres"} of wall.
         </p>
+
         <p>
           In order for your drywall to line up right, the second stud is placed at{" "}
           {isImperialUnit ? "15.25 inches" : "387 milimetres"}. From there, you can hook
@@ -90,10 +98,14 @@ const Calculator = () => {
           Your wall is shown below, placing the edge of each stud on the measurments
           listed.
         </p>
-        <p>Place your studs at: {listOfMeasurements.join(", ")}</p>
+
+        <p>
+          Place your studs at:
+          {listOfMeasurements.join(", ")}
+        </p>
       </Card>
     </>
   );
-};
+}
 
 export default Calculator;
