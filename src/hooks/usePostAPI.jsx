b32a3e7c3871b25ec15885dbb290978d43e6188c
UnrulyMob <72175425+JPM709@users.noreply.github.com>
2021-10-10 12:51:27 -0230

    Revert "Update usePostAPI.jsx"
    This reverts commit 17ce79e5938fc2e4c67ede1a5f03417cd06a100d.

--- src/hooks/usePostAPI.jsx
+++ src/hooks/usePostAPI.jsx
@@ -5,6 +5,31 @@ export default function usePostAPI() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
+<<<<<<< Updated upstream
+  const callAPI = useCallback(
+    async (url, bodyJSON) => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.Token}`,
+          },
+          body: JSON.stringify(bodyJSON),
+        });
+        const json = await res.json();
+        setData(json);
+        setIsLoading(false);
+        return json;
+      } catch (err) {
+        setError(err.message);
+      }
+      return null;
+    },
+    []
+  );
+=======
   const callAPI = useCallback(async (url, bodyJSON) => {
     setIsLoading(true);
     try {
@@ -25,6 +50,7 @@ export default function usePostAPI() {
     }
     return null;
   }, []);
+>>>>>>> Stashed changes
 
   return [{data, isLoading, error}, callAPI];
 }
